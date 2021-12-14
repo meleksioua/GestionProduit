939d8cf9fc60677cb74f200863b0655fb3ba9293
Melek Sioua <meleksioua23@gmail.com>
2021-12-14 11:01:29 +0100

    create object P1 in GP.Console

--- GP.Console/Program.cs
+++ GP.Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GP.Domain;
 
 namespace GP.Console
 {
@@ -10,6 +11,8 @@ namespace GP.Console
     {
         static void Main(string[] args)
         {
+            Provider P1 = new Provider();
+            
         }
     }
 }
